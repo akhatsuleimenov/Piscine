f5157ffd152335fa4fa6777e5e87ef625c1a50b6
Akhat09 <akhatsuleimenov@gmail.com>
2019-08-14 18:20:13 +0200

    Add day 07

---
+++ 42_piscine-day07/ex05/ft_print_words_tables.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_words_tables.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aksuleim <aksuleim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/18 10:33:19 by aksuleim          #+#    #+#             */
+/*   Updated: 2019/07/18 11:30:14 by aksuleim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c);
+
+void	ft_print_words_tables(char **tab)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (tab[i])
+	{
+		j = 0;
+		while (tab[i][j])
+		{
+			ft_putchar(tab[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
