353364c22f52d4ccaf1cba2143351a7be4d109b5
Akhat09 <akhatsuleimenov@gmail.com>
2019-08-14 18:18:03 +0200

    Add day 01

---
+++ 42_piscine-day01/ex08/add_chelou.sh
@@ -0,0 +1,1 @@
+FT_NBR1="$(echo $FT_NBR1 | tr \''\\\"\?\!' '01234')";FT_NBR2="$(echo $FT_NBR2 | tr 'mrdoc' '01234')";RESULT=$(echo "obase=13;ibase=5;$FT_NBR1+$FT_NBR2" | bc);echo $RESULT | tr '0123456789ABC' 'gtaio luSnemf'
